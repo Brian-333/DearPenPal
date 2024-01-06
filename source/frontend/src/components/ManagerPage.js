9786ddd270d4bd0c64dc892c647a87d1d6b71bbf
Josiah Korzan <josiahkorzan4@gmail.com>
2024-01-06 13:53:20 -0700

    Created Navbar and Table for ManagerPage component

--- source/frontend/src/components/ManagerPage.js
+++ source/frontend/src/components/ManagerPage.js
@@ -1,21 +1,44 @@
-const ManagerPage = () => {
-  const styles = {
-    backgroundColor: 'black',
-    padding: 20
-  };
+import '../styles/managerPage.css';
 
+const ManagerPage = () => {
   return (
-    <nav style={styles}>
+    <div>
+      <NavBar/>
+      <table>
+        <tr>
+          <th>Name</th>
+          <th>Username</th>
+          <th>Password</th>
+        </tr>
+        <StudentRow name="John" username="john123" password="banana"/>
+      </table>
+    </div>
+  );
+}
+
+const StudentRow = (props) => {
+  return (
+    <tr>
+      <td>{props.name}</td>
+      <td>{props.username}</td>
+      <td>{props.password}</td>
+    </tr>
+  );
+}
+
+const NavBar = () => {
+  return (
+    <nav>
       <div className="account-container">
         <button>Account</button>
       </div>
       <div className="search-container">
         <form>
-          <input type="text" placeholder="Search..." />
+          <input type="text" placeholder="Search for Student..." />
           <button type="submit">Search</button>
         </form>
       </div>
-  </nav>
+    </nav>
   );
 }
 
