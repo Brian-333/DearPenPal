ac8ae1943ff4f48784982bcb94b0cfe50a888f29
Kelsey <kelseypattison1@gmail.com>
2024-01-06 12:43:52 -0700

    Login pages

--- source/frontend/src/App.js
+++ source/frontend/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from './components/LandingPage'
+import ManagerLogin from './components/ManagerLogin'
+import PersonLogin from './components/PersonLogin'
 import './App.css';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={ <LandingPage/> }></Route>
+      <Route path="/" element={ <PersonLogin/> }></Route>
     </Routes>
   );
 }
