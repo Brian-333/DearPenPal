cc4bb75bf13147f8631b81ee21471fbc931da010
Josiah Korzan <josiahkorzan4@gmail.com>
2024-01-06 12:50:48 -0700

    Working on ManagerPage.js

--- source/frontend/src/App.js
+++ source/frontend/src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import LandingPage from './components/LandingPage'
+import ManagerPage from './components/ManagerPage'
 import './App.css';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={ <LandingPage/> }></Route>
+      <Route path="/manager" element={ <ManagerPage/> }></Route>
     </Routes>
   );
 }
