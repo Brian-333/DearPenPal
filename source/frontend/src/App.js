706851d2b1c991328a2e777b0b32644ced752f0e
Kelsey <kelseypattison1@gmail.com>
2024-01-06 20:01:35 -0700

    send page

--- source/frontend/src/App.js
+++ source/frontend/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from './components/LandingPage'
 import ManagerPage from './components/ManagerPage'
 import InboxPage from './components/InboxPage'
+import SendPage from './components/SendPage'
 import './App.css';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={ <LandingPage/> }></Route>
       <Route path="/Manager" element={ <ManagerPage/> }></Route>
       <Route path= "/Inbox" element={<InboxPage/>}></Route>
+      <Route path= "/Send" element={<SendPage/>}></Route>
     </Routes>
   );
 }
---
+++ source/frontend/src/components/SendPage.js
@@ -0,0 +1,50 @@
+import '../styles/Inbox.css'
+
+const SendPage = () => {
+    return(
+        <div class = 'inboxbg'>
+            <button class = 'inboxbbutton'>Back</button>
+            <table class = 'tabletop'>
+                <tr>
+                    <th>Name</th>
+                    <th>Date</th>
+                    <th>Open</th>
+                </tr>
+            </table>
+            <div class = 'sidebyside'>
+                <div class = "inletter">
+                <table>
+                    <Letter name = "John" date = '2024-01-06'></Letter>
+                    <Letter name = "John" date = '2024-01-06' ></Letter>
+                    <Letter name = "John" date = '2024-01-06' ></Letter>
+                    <Letter name = "John" date = '2024-01-06' ></Letter>
+                    <Letter name = "John" date = '2024-01-06' ></Letter>
+                    <Letter name = "John" date = '2024-01-06' ></Letter>
+                    <Letter name = "John" date = '2024-01-06' ></Letter>
+                    <Letter name = "John" date = '2024-01-06' ></Letter>
+                    <Letter name = "John" date = '2024-01-06' ></Letter>
+                    <Letter name = "John" date = '2024-01-06' ></Letter>
+                </table>
+                </div>
+                <div class = 'letterviewer'>
+                    <input type= 'text' class = 'sendletter'></input>
+                </div>
+            </div>
+            <div class = 'Send Button'>
+                <button class = 'inboxbbutton'>Send</button>
+            </div>
+        </div>
+    );
+}
+
+const Letter = (props) => {
+    return (
+        <tr>
+            <td>To: {props.name}</td>
+            <td>{props.date}</td>
+            <td><button id = {props.name}>Open</button></td>
+        </tr>
+    );
+} 
+
+export default SendPage
