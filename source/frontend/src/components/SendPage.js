e04b096952d9276ae630cff821d5f564c580e640
Kelsey <kelseypattison1@gmail.com>
2024-01-07 00:54:57 -0700

    added logout button to send page

--- source/frontend/src/components/SendPage.js
+++ source/frontend/src/components/SendPage.js
@@ -4,11 +4,8 @@ const SendPage = () => {
     return (
         <div class = 'inboxbg'>
             <button class = 'inboxbbutton'>Back</button>
-            <label class = 'waywtlabel'>Who are you writing to: </label>
-            <select class = 'selectperson'>
-                <option value = 'Kid1'>kid 1</option>
-                <option value = 'kid2'>kid 2</option>
-            </select>
+            <button class = 'logoutbutton'>Logout</button>
+            <button class = 'readlettersbutton'>Read Your Letters</button>
             <div class = 'sidebyside'>
                 <div>
                     <textarea rows = '80' cols = '5' class = 'sendletter'></textarea>
